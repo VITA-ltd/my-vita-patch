089b15d7a8beb606ebcb2a75c8ad231f08fc52b8
Jayden Tucker <jaydenrtucker@gmail.com>
2024-09-20 21:10:36 -0700

    fixed the header blur trigger on the shop page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -35,7 +35,7 @@ export function Header({
         scrollActivation = (window.innerHeight / 2) - 60;
       }
 
-      if (window.scrollY > scrollActivation || window.location.href !== '/') {
+      if (window.scrollY > scrollActivation || window.location.pathname !== '/') {
         setBackgroundActive(true);
       } else {
         setBackgroundActive(false);
